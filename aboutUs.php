4ba60335ab5640f186c8e42a773fc8bf64cf2c58
Afshan <afshan25ali@gmail.com>
2024-05-29 15:26:44 +0530

    Add files via upload

---
+++ aboutUs.php
@@ -0,0 +1,74 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+	
+	<!-- HEAD TAG STARTS -->
+
+	<head>
+	
+  		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	
+		<title>About Us | My-Tour</title>
+    
+    	<link href="css/main.css" rel="stylesheet">
+    	<link href="css/bootstrap.min.css" rel="stylesheet">
+    	<link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400|Raleway:100,300,400,500|Roboto:100,400,500,700" rel="stylesheet">
+    	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    
+    	<script src="js/jquery-3.2.1.min.js"></script>
+    	<script src="js/main.js"></script>
+    	<script src="js/bootstrap.min.js"></script>
+    	
+	</head>
+	
+	<!-- HEAD TAG ENDS -->
+	
+	<!-- BODY TAG STARTS -->
+	
+	<body>
+		
+		<?php 
+		
+			if(!isset($_SESSION["username"])) {
+				include("common/headerLoggedOut.php");
+			}
+			else {
+				include("common/headerLoggedIn.php");
+			}
+		
+		?>
+		
+		<div class="spacer">a</div>
+		
+		<div class="col-sm-12 aboutUsWrapper">
+			
+			<div class="headingOne">
+				
+				About Us
+				
+			</div>
+			
+			<div class="para">
+				<!--dummy description---->
+				My-Tour is a web application designed and developed which aimed at computerizing all its official functions to ensure simple operations. It covers wide circle of  Tourism  and its related services and ensures smooth running of operations with respect to the database.
+
+Manual maintenance of database does not ensure security and consumes more time when looking for data or reports. Similarly editing of data performed manually causes more inconvenience and record maintenance becomes very difficult. The above package reduces errors and flows created manually while booking for a service.
+
+In this project, we are using Visual Studio Code to mainly design the front end of the project and the back end is implemented on XAMPP Web Server. Web Server software is used to design back end mainly because we can create relational table employee database system easily; using Web Server and Visual Studio Code software, provide a good graphical user interface to the user of the system
+
+			</div>
+			
+		</div> <!-- paymentWrapper -->
+	
+	<div class="spacerLarge">.</div> <!-- just a dummy class for creating some space -->
+			
+		<?php include("common/footer.php"); ?>
+				
+	</body>
+	
+	<!-- BODY TAG ENDS -->
+	
+</html>
